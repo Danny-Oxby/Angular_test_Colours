0486a08385939a27b42cccebc5173bdc97aac6ff
Danny Oxby <oxbyd@assayoffice.co.uk>
2022-07-12 12:24:36 +0100

    tested the application and binding of multiple diffrent types of input on numbers.component

--- Angular_test_project/ClientApp/src/app/number/number.component.ts
+++ Angular_test_project/ClientApp/src/app/number/number.component.ts
@@ -7,9 +7,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NumberComponent implements OnInit {
 
+  ftext: string = '';
+  fnumber: number = 0;
+  fdate: number = Date.now();
+  femail: string = '';
+  fpassword: string = '';
+
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  Check(): void {
+    this.ftext = this.ftext;
+    this.fnumber = this.fnumber;
+    this.fdate = this.fdate;
+    this.femail = this.femail;
+    this.fpassword = this.fpassword;
+  }
 }
